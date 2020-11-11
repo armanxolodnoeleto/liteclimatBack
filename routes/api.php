da12dfee7e1cadcfabe2495012215fb1e472acdc
armanxolodnoeleto <avdey@xolodnoeleto.ru>
2020-11-11 18:28:05 +0400

    change post to get

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('getCategories', 'CategoriesController@getCategories');
+Route::get('getCategories', 'CategoriesController@getCategories');
