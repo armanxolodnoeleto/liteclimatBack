5935e90d60592d7c1102fefbd27fbdee68c8b11e
armanxolodnoeleto <avdey@xolodnoeleto.ru>
2020-11-12 17:48:46 +0400

    product controller

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('getCategories', 'CategoriesController@getCategories');
-Route::post('getProducts', 'ProductsController@getProducts');
+Route::get('getCategories', 'CategoriesController@getCategories');
+Route::get('getProducts', 'ProductsController@getProducts');
