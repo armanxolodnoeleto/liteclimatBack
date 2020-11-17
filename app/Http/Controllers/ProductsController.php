2caae9d88af7948b640dd8b5f3950e4a98381e46
armanxolodnoeleto <avdey@xolodnoeleto.ru>
2020-11-17 18:42:45 +0400

    getproducts changes

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\Type;
 
 class ProductsController extends Controller
 {
     public function getProducts(Request $request, $categoryId) {
         $projectId = $request->header('projectId');
-        $page = $request->page;
-        $skip = ($page - 1) * 10;
+        $data = [];
 
         $products = DB::table('products_by_projects')
             ->join('product_to_categories', 'products_by_projects.product_id', '=', 'product_to_categories.product_id')
@@ -28,11 +28,13 @@ class ProductsController extends Controller
             ->where('product_characteristics.characteristic_id', 3)
             ->where('product_series_photos.cover_photo', 1)
             ->where('photos.cover_photo', 1)
-            ->select('products.id', 'products.name as model', 'product_manufacturers.name as brand', 'product_manufacturers.logo as brand_logo', 'product_series.series_name_ru as series_name', 'product_series_photos.folder as series_picture_folder', 'product_series_photos.file_name as series_picture_file_name', 'product_series_photos.file_format as series_picture_format', 'photos.folder as product_picture_folder', 'photos.file_name as product_picture_file_name', 'photos.file_format as product_picture_format', 'prices.price', 'prices.setup_price', 'characteristics.name_ru as characteristic_name_ru', 'characteristic_attributes.name_ru as characteristic_attribute_name')
-            ->skip($skip)
-            ->take(10)
-            ->get();
+            ->select('products.id', 'products.name as model', 'product_manufacturers.name as brand', 'product_manufacturers.logo as brand_logo', 'product_series.series_name_ru as series_name', 'product_series_photos.folder as series_picture_folder', 'product_series_photos.file_name as series_picture_file_name', 'product_series_photos.file_format as series_picture_format', 'photos.folder as product_picture_folder', 'photos.file_name as product_picture_file_name', 'photos.file_format as product_picture_format', 'prices.price', 'prices.setup_price', 'characteristics.name_ru as characteristic_name_ru', 'characteristic_attributes.name_ru as characteristic_attribute_name'/*, DB::raw('count(*) as total')*/)
+            ->paginate(10);
 
-        return response()->json($products);
+
+        $data['products_info']['total'] = $products->total();
+        $data['products'] = $products->items();
+
+        return response()->json($data);
     }
 }
