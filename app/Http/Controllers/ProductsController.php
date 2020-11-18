2bc2b7d4fe98ba65cb5b6d8744268afc17738040
armanxolodnoeleto <avdey@xolodnoeleto.ru>
2020-11-18 18:28:25 +0400

    products query

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -14,25 +14,29 @@ class ProductsController extends Controller
 
         $products = DB::table('products_by_projects')
             ->join('product_to_categories', 'products_by_projects.product_id', '=', 'product_to_categories.product_id')
-            ->join('products', 'products_by_projects.product_id', '=', 'products.id')
+            ->join('products', 'product_to_categories.product_id', '=', 'products.id')
             ->leftJoin('product_manufacturers', 'products.manufacturer_id', '=', 'product_manufacturers.id')
             ->leftJoin('product_series', 'products.series_id', '=', 'product_series.id')
             ->leftJoin('product_series_photos', 'product_series.id', '=', 'product_series_photos.id')
             ->leftJoin('photos', 'products.id', '=', 'photos.product_id')
             ->leftJoin('prices', 'products.id', '=', 'prices.product_id')
-            ->leftJoin('product_characteristics', 'products.id', '=', 'product_characteristics.product_id')
-            ->leftJoin('characteristics', 'product_characteristics.characteristic_id', '=', 'characteristics.id')
-            ->leftJoin('characteristic_attributes', 'product_characteristics.attribute_id', '=', 'characteristic_attributes.id')
             ->where('products_by_projects.project_id', $projectId)
             ->where('product_to_categories.category_id', $categoryId)
-            ->where('product_characteristics.characteristic_id', 3)
-            ->where('product_series_photos.cover_photo', 1)
-            ->where('photos.cover_photo', 1)
-            ->select('products.id', 'products.name as model', 'product_manufacturers.name as brand', 'product_manufacturers.logo as brand_logo', 'product_series.series_name_ru as series_name', 'product_series_photos.folder as series_picture_folder', 'product_series_photos.file_name as series_picture_file_name', 'product_series_photos.file_format as series_picture_format', 'photos.folder as product_picture_folder', 'photos.file_name as product_picture_file_name', 'photos.file_format as product_picture_format', 'prices.price', 'prices.setup_price', 'characteristics.name_ru as characteristic_name_ru', 'characteristic_attributes.name_ru as characteristic_attribute_name'/*, DB::raw('count(*) as total')*/)
+            ->select('products.id', 'products.name as model', 'product_manufacturers.name as brand', 'product_manufacturers.logo as brand_logo', 'product_series.series_name_ru as series_name', 'product_series_photos.folder as series_picture_folder', 'product_series_photos.file_name as series_picture_file_name', 'product_series_photos.file_format as series_picture_format', 'photos.folder as product_picture_folder', 'photos.file_name as product_picture_file_name', 'photos.file_format as product_picture_format', 'prices.price', 'prices.setup_price')
             ->paginate(10);
 
+        $productIds = $products->pluck('id');
+
+        $characteristics = DB::table('product_characteristics')
+            ->leftJoin('characteristics', 'product_characteristics.characteristic_id', '=', 'characteristics.id')
+            ->leftJoin('characteristic_attributes', 'product_characteristics.attribute_id', '=', 'characteristic_attributes.id')
+            ->whereIn('product_id',$productIds)
+            ->where('product_characteristics.characteristic_id', 3)
+            ->select('product_characteristics.product_id as id', 'characteristics.name_ru as characteristic_name_ru','characteristic_attributes.name_ru as characteristic_attribute_name')
+            ->get()->toArray();
 
         $data['products_info']['total'] = $products->total();
+        $data['products_info']['characteristics'] = $characteristics;
         $data['products'] = $products->items();
 
         return response()->json($data);
