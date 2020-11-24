45706edebd660ca115a1d9c1f78bd4235e6b372f
armanxolodnoeleto <avdey@xolodnoeleto.ru>
2020-11-24 18:04:05 +0400

    product

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use phpDocumentor\Reflection\Type;
 
 class ProductsController extends Controller
 {
@@ -17,13 +16,14 @@ class ProductsController extends Controller
             ->join('products', 'product_to_categories.product_id', '=', 'products.id')
             ->leftJoin('product_manufacturers', 'products.manufacturer_id', '=', 'product_manufacturers.id')
             ->leftJoin('product_series', 'products.series_id', '=', 'product_series.id')
-            ->leftJoin('product_series_photos', 'product_series.id', '=', 'product_series_photos.id')
+            ->leftJoin('product_series_photos', 'product_series.id', '=', 'product_series_photos.series_id')
             ->leftJoin('photos', 'products.id', '=', 'photos.product_id')
             ->leftJoin('prices', 'products.id', '=', 'prices.product_id')
             ->where('products_by_projects.project_id', $projectId)
             ->where('product_to_categories.category_id', $categoryId)
             ->where('prices.status', 1)
             ->where('prices.price', '!=', 0)
+            ->where('product_series_photos.cover_photo', '=', 1)
             ->select('products.id', 'products.name as model', 'product_manufacturers.name as brand', 'product_manufacturers.logo as brand_logo', 'product_series.series_name_ru as series_name', 'product_series_photos.folder as series_picture_folder', 'product_series_photos.file_name as series_picture_file_name', 'product_series_photos.file_format as series_picture_format', 'photos.folder as product_picture_folder', 'photos.file_name as product_picture_file_name', 'photos.file_format as product_picture_format', 'prices.price', 'prices.setup_price')
             ->paginate(10);
 
@@ -40,6 +40,49 @@ class ProductsController extends Controller
         $data['products_info']['total'] = $products->total();
         $data['products_info']['characteristics'] = $characteristics;
         $data['products'] = $products->items();
+
+        return response()->json($data);
+    }
+
+    public function getProduct(Request $request, $productId) {
+        $projectId = $request->header('projectId');
+        $data = [];
+        $product = DB::table('products')
+            ->leftJoin('prices', 'products.id', '=', 'prices.product_id')
+            ->leftJoin('product_manufacturers', 'products.manufacturer_id', '=', 'product_manufacturers.id')
+            ->leftJoin('product_manufacturer_certificates', 'product_manufacturers.id', '=', 'product_manufacturer_certificates.product_manufacturer_id')
+            ->leftJoin('product_series', 'products.series_id', '=', 'product_series.id')
+            ->leftJoin('product_series_photos', 'product_series.id', '=', 'product_series_photos.series_id')
+            ->where('prices.project_id', $projectId)
+            ->where('products.id', $productId)
+            ->select('products.name as model', 'products.id as articule', 'products.description_ru as description', 'prices.market', 'prices.setup_price', 'prices.price', 'product_manufacturers.name as brand', 'product_manufacturers.logo as manufacturer_logo', 'product_manufacturer_certificates.folder as certificate_folder', 'product_manufacturer_certificates.file_name as certificate_file_name', 'product_manufacturer_certificates.file_format as certificate_file_format', 'product_series.series_name_ru as series_name', 'product_series.id as series_id')
+            ->groupBy('products.id', 'products.name', 'prices.market', 'prices.setup_price', 'prices.price', 'product_manufacturers.name', 'product_manufacturers.logo', 'product_manufacturer_certificates.folder', 'product_manufacturer_certificates.file_name', 'product_manufacturer_certificates.file_format', 'product_series.series_name_ru', 'product_series_photos.folder', 'product_series_photos.file_format', 'products.description_ru', 'product_series.id')
+            ->first();
+
+        $characteristics = DB::table('product_characteristics')
+            ->leftJoin('characteristics as ch1', 'product_characteristics.characteristic_id', '=', 'ch1.id')
+            ->leftJoin('characteristics as ch2', 'ch1.parent_id', '=', 'ch2.id')
+            ->leftJoin('characteristic_attributes', 'product_characteristics.attribute_id', '=', 'characteristic_attributes.id')
+            ->where('product_id',$productId)
+            ->select('product_characteristics.value as characteristic_value','ch1.name_ru as characteristic_name','characteristic_attributes.name_ru as characteristic_attribute_name', 'ch2.name_ru as title')
+            ->get()
+            ->toArray();
+
+        $photos = DB::table('photos')
+            ->where('product_id', $productId)
+            ->get()
+            ->toArray();
+
+        if (empty($photos)) {
+            $seriesId = $product->series_id;
+            $photos = DB::table('product_series_photos')
+                ->where('series_id', $seriesId)
+                ->get()->toArray();
+        }
+
+        $data['product'] = $product;
+        $data['characteristics'] = $characteristics;
+        $data['photos'] = $photos;
 
         return response()->json($data);
     }
