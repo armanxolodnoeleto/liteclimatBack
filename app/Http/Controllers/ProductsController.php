87019850541119f6c7881d79f407ff8baa0b94af
Your Name <you@example.com>
2020-12-02 16:22:38 +0400

    checkbox filter

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -10,29 +10,45 @@ class ProductsController extends Controller
     public function getProducts(Request $request, $categoryId) {
         $projectId = $request->header('projectId');
         $data = [];
-
         $query = DB::table('products_by_projects')
-            ->join('product_to_categories', 'products_by_projects.product_id', '=', 'product_to_categories.product_id')
-            ->join('products', 'product_to_categories.product_id', '=', 'products.id')
+            ->leftJoin('product_to_categories', 'products_by_projects.product_id', '=', 'product_to_categories.product_id')
+            ->leftJoin('products', 'product_to_categories.product_id', '=', 'products.id')
             ->leftJoin('product_manufacturers', 'products.manufacturer_id', '=', 'product_manufacturers.id')
             ->leftJoin('product_series', 'products.series_id', '=', 'product_series.id')
             ->leftJoin('product_series_photos', 'product_series.id', '=', 'product_series_photos.series_id')
             ->leftJoin('photos', 'products.id', '=', 'photos.product_id')
-            ->leftJoin('prices', 'products.id', '=', 'prices.product_id')
-            ->where('products_by_projects.project_id', $projectId)
-            ->where('product_to_categories.category_id', $categoryId)
-            ->where('prices.status', 1)
-            ->where('prices.price', '!=', 0)
-            ->where('product_series_photos.cover_photo', '=', 1);
+            ->leftJoin('prices', 'products.id', '=', 'prices.product_id');
 
-
-        if ($request->has('manufacturerCountries')) {
-            $manufacturers = clone $query;
-            $query = $manufacturers->whereIn('product_manufacturers.id', $request->manufacturerCountries);
+        $filterData = $request->except('manufacturerCountries');
+        if ($request->has('checkboxes')) {
+            $query = $query->leftJoin('product_characteristics', 'products.id', '=', 'product_characteristics.product_id');
         }
 
-        $products = $query->select('products.id', 'products.name as model', 'product_manufacturers.name as brand', 'product_manufacturers.logo as brand_logo', 'product_series.series_name_ru as series_name', 'product_series_photos.folder as series_picture_folder', 'product_series_photos.file_name as series_picture_file_name', 'product_series_photos.file_format as series_picture_format', 'photos.folder as product_picture_folder', 'photos.file_name as product_picture_file_name', 'photos.file_format as product_picture_format', 'prices.price', 'prices.setup_price')
+        $query = $query->where('prices.project_id', $projectId)
+            ->where('product_to_categories.category_id', $categoryId)
+            ->where('prices.status', 1)
+            ->where('product_series_photos.cover_photo', '=', 1)
+            ->where('prices.price', '!=', 0);
+
+        if ($request->has('manufacturerCountries')) {
+            $query = $query->whereIn('product_manufacturers.id', $request->manufacturerCountries);
+        }
+
+        if ($request->has('checkboxes')) {
+            $checkboxes = $filterData['checkboxes'];
+            $query = $query->where(function ($q) use ($checkboxes) {
+                foreach ($checkboxes as $key => $item) {
+                    $q->orWhere('product_characteristics.characteristic_id', $key)->whereIn('product_characteristics.attribute_id', $item);
+                }
+            });
+        }
+
+        $products = $query->select('products.id', 'products.name as model', 'product_manufacturers.name as brand', 'product_manufacturers.logo as brand_logo', 'product_series.series_name_ru as series_name', 'product_series_photos.folder as series_picture_folder', 'product_series_photos.file_name as series_picture_file_name','product_series_photos.file_format as series_picture_format','photos.folder as product_picture_folder','photos.file_name as product_picture_file_name', 'photos.file_format as product_picture_format', 'prices.price', 'prices.setup_price')
+            ->distinct()
             ->paginate(10);
+//            ->groupBy('products.id', 'products.name', 'product_manufacturers.name', 'product_manufacturers.logo', 'product_series.series_name_ru', 'product_series_photos.folder', 'product_series_photos.file_name','product_series_photos.file_format','photos.folder','photos.file_name', 'photos.file_format', 'prices.price', 'prices.setup_price');
+
+//        return $products->paginate(10);
 
         $productIds = $products->pluck('id');
 
