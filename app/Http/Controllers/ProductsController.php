0803883a53fc4e3c70153b6b2b17321ea1b7d4ce
Your Name <you@example.com>
2020-12-22 14:45:08 +0400

    cover photo

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -14,8 +14,8 @@ class ProductsController extends Controller
             ->leftJoin('product_to_categories', 'prices.product_id', '=', 'product_to_categories.product_id')
             ->leftJoin('products', 'product_to_categories.product_id', '=', 'products.id')
             ->leftJoin('product_manufacturers', 'products.manufacturer_id', '=', 'product_manufacturers.id')
-            ->leftJoin('product_series', 'products.series_id', '=', 'product_series.id')
-            ->leftJoin('product_series_photos', 'product_series.id', '=', 'product_series_photos.series_id')
+            ->join('product_series', 'products.series_id', '=', 'product_series.id', 'left outer')
+            ->join('product_series_photos', 'product_series.id', '=', 'product_series_photos.series_id', 'left outer')
             ->leftJoin('photos', 'products.id', '=', 'photos.product_id');
 
         $filterData = $request->except('manufacturerCountries');
@@ -28,7 +28,7 @@ class ProductsController extends Controller
         $query = $query->where('prices.project_id', $projectId)
             ->where('product_to_categories.category_id', $categoryId)
             ->where('prices.status', 1)
-            ->where('product_series_photos.cover_photo', '=', 1)
+//            ->where('product_series_photos.cover_photo', '=', 1)
             ->where('prices.price', '!=', 0);
 
         if ($request->has('manufacturerCountries')) {
@@ -83,7 +83,8 @@ class ProductsController extends Controller
             }
         }
 
-        $query = $query->select('products.id', 'products.name as model', 'product_manufacturers.name as brand', 'product_manufacturers.logo as brand_logo', 'product_series.series_name_ru as series_name', 'product_series_photos.folder as series_picture_folder', 'product_series_photos.file_name as series_picture_file_name','product_series_photos.file_format as series_picture_format','photos.folder as product_picture_folder','photos.file_name as product_picture_file_name', 'photos.file_format as product_picture_format', 'prices.price', 'prices.setup_price')
+        $query = $query->select('products.id', 'products.name as model', 'product_manufacturers.name as brand', 'product_manufacturers.logo as brand_logo', 'product_series.series_name_ru as series_name', DB::raw('IF (product_series_photos.cover_photo IS NULL, NULL, CONCAT("[", GROUP_CONCAT(JSON_OBJECT( "cover_photo", product_series_photos.cover_photo,"series_picture_folder",  product_series_photos.folder, "series_picture_file_name", product_series_photos.file_name, "series_picture_format", product_series_photos.file_format)), "]")) as cover_photo'), 'photos.folder as product_picture_folder','photos.file_name as product_picture_file_name', 'photos.file_format as product_picture_format', 'prices.price', 'prices.setup_price')
+            ->groupBy('products.id', 'products.name', 'product_manufacturers.name', 'product_manufacturers.logo', 'product_series.series_name_ru', 'photos.folder','photos.file_name', 'photos.file_format', 'prices.price', 'prices.setup_price', 'product_series_photos.cover_photo')
             ->distinct();
 
         if ($request->has('orderBy')) {
