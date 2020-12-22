7079319ca8606207bc3d9bf867f60c134fcb1737
Your Name <you@example.com>
2020-12-22 15:16:55 +0400

    new changes

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -83,8 +83,8 @@ class ProductsController extends Controller
             }
         }
 
-        $query = $query->select('products.id', 'products.name as model', 'product_manufacturers.name as brand', 'product_manufacturers.logo as brand_logo', 'product_series.series_name_ru as series_name', DB::raw('IF (product_series_photos.cover_photo IS NULL, NULL, CONCAT("[", GROUP_CONCAT(JSON_OBJECT( "cover_photo", product_series_photos.cover_photo,"series_picture_folder",  product_series_photos.folder, "series_picture_file_name", product_series_photos.file_name, "series_picture_format", product_series_photos.file_format)), "]")) as cover_photo'), 'photos.folder as product_picture_folder','photos.file_name as product_picture_file_name', 'photos.file_format as product_picture_format', 'prices.price', 'prices.setup_price')
-            ->groupBy('products.id', 'products.name', 'product_manufacturers.name', 'product_manufacturers.logo', 'product_series.series_name_ru', 'photos.folder','photos.file_name', 'photos.file_format', 'prices.price', 'prices.setup_price', 'product_series_photos.cover_photo')
+        $query = $query->select('products.id', 'products.name as model', 'product_manufacturers.name as brand', 'product_manufacturers.logo as brand_logo', 'product_series.series_name_ru as series_name', DB::raw('CONCAT("[", GROUP_CONCAT(JSON_OBJECT( "cover_photo", product_series_photos.cover_photo,"series_picture_folder",  product_series_photos.folder, "series_picture_file_name", product_series_photos.file_name, "series_picture_format", product_series_photos.file_format), "]")) as cover_photo'), 'photos.folder as product_picture_folder','photos.file_name as product_picture_file_name', 'photos.file_format as product_picture_format', 'prices.price', 'prices.setup_price')
+            ->groupBy('products.id', 'products.name', 'product_manufacturers.name', 'product_manufacturers.logo', 'product_series.series_name_ru', 'photos.folder','photos.file_name', 'photos.file_format', 'prices.price', 'prices.setup_price')
             ->distinct();
 
         if ($request->has('orderBy')) {
@@ -221,7 +221,7 @@ class ProductsController extends Controller
                 ->leftJoin('photos', 'products.id', '=', 'photos.product_id')
                 ->where('prices.project_id', $projectId)
                 ->where('prices.status', 1)
-                ->where('product_series_photos.cover_photo',1)
+//                ->where('product_series_photos.cover_photo',1)
                 ->where('prices.price', '!=', 0)
                 ->where(function($q) use ($searchableColumns, $searchBy) {
                     foreach ($searchableColumns as $searchableColumn) {
